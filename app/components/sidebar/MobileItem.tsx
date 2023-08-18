4d0330eeebe960888a4700dbfb17a46dad208196
U-Cant-Find-Me <anirbanmoi.nitdgp.19u10573@gmail.com>
2023-08-18 18:09:12 +0530

    Error_Fix_2

--- app/components/sidebar/MobileItem.tsx
+++ app/components/sidebar/MobileItem.tsx
@@ -1,5 +1,3 @@
-'use client';
-
 import clsx from "clsx";
 import Link from "next/link";
 
@@ -23,10 +21,10 @@ const MobileItem: React.FC<MobileItemProps> = ({
     }
 
     return (
-        <Link onClick={onClick} href={href} className={clsx(`gorup flex gap-x-3 text-sm leading-6 font-semibold w-full justify-center p-4 text-gray-500 hover:text-black hover:bg-gray-100`,
-        active && "bg-gray-100 text-black"
+        <Link onClick={handleClick} href={href} className={clsx(`gorup flex gap-x-3 text-sm leading-6 font-semibold w-full justify-center p-4 text-gray-500 hover:text-black hover:bg-gray-100`,
+        active && "bg-gray-100 text-black",
         )}>
-            <Icon className="h-6 w-6" />
+        <Icon className="h-6 w-6" />
         </Link>
     )
 }
