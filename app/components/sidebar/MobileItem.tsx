3895dd840f54c8870c0743bf5c930fbacf17856e
U-Cant-Find-Me <anirbanmoi.nitdgp.19u10573@gmail.com>
2023-08-18 19:02:23 +0530

    Error_Fix_3

--- app/components/sidebar/MobileItem.tsx
+++ app/components/sidebar/MobileItem.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 interface MobileItemProps {
     href: string;
-    icon: string;
+    icon: any;
     active?: boolean;
     onClick?: () => void;
 }
@@ -24,7 +24,7 @@ const MobileItem: React.FC<MobileItemProps> = ({
         <Link onClick={handleClick} href={href} className={clsx(`gorup flex gap-x-3 text-sm leading-6 font-semibold w-full justify-center p-4 text-gray-500 hover:text-black hover:bg-gray-100`,
         active && "bg-gray-100 text-black",
         )}>
-        <Icon className="h-6 w-6" />
+            <Icon className="h-6 w-6" />
         </Link>
     )
 }
